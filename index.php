55dc52be75c4d0949ddf1f533fd4d118e5798530
Tristan White <oliver.tristan@gmail.com>
2023-11-09 10:29:33 +0100

    Add support for multilayered chats

--- index.php
+++ index.php
@@ -18,6 +18,16 @@ $ai = new ChatGPT;
 // echo "<p>".$ai->textGeneration('who is Tristan White?')->choices[0]->message->content."</p>";
 
 
+// Chat Generation
+// $chats = [
+//     ["role" => "system", "content" => "You are a helpful assistant." ], 
+//     ["role" => "user", "content" => "Who won the world series in 2020?" ], 
+//     ["role" => "assistant", "content" => "The Los Angeles Dodgers won the World Series in 2020." ], 
+//     ["role" => "user", "content" => "Where was it played?" ] 
+// ];
+// echo $ai->chatGeneration($chats)->choices[0]->message->content;
+
+
 // Image Generation
 // $image = $ai->imageGeneration('Draw Tristan White as a magnificent robot'); 
 // echo '<img src="'.$image->data[0]->url.'" alt="'.$image->data[0]->revised_prompt.'">';
